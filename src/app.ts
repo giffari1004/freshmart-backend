f7ae3b3c6c7da46fd6bea08216067f0d043bb19c
nicholasng <nicholasngnicholasng29206@gmail.com>
2026-08-01 12:40:20 +0700

    feat(admin): add store admin CRUD with validation and bcrypt

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import rateLimit from "express-rate-limit";
 import { WHITE_LIST } from "./configs/env-config";
 import { ErrorMiddleware } from "./middlewares/error-middleware";
 import { NotFoundError } from "./errors/NotFoundError";
-import { AdminRouter } from "./features/admin/admin-route";
+import { adminRouter } from "./features/admin/admin-route";
 
 const app = express();
 
@@ -39,7 +39,7 @@ app.get("/", (_req, res) => {
 // Routes
 // app.use('/api/products', ProductRoutes);
 // app.use('/api/auth', AuthRoutes);
-app.use('/api/v1' , AdminRouter)
+app.use('/api/v1' , adminRouter)
 
 app.use((_req, _res, next) => {
   next(new NotFoundError("Endpoint not found"));
---
+++ src/features/admin/admin-validation.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+import { Role } from "../../../generated/prisma";
+import { USER_SORT_BY, USER_SORT_ORDER } from "./admin-constant";
+export class AdminValidation {
+  static readonly GET_ALL_USER = z.object({
+    query: z.object({
+      page: z.coerce.number().int().positive().optional().default(1),
+      limit: z.coerce.number().int().positive().max(100).optional().default(10),
+      search: z.preprocess(
+        (val) => (val === "" ? undefined : val),
+        z.string().trim().optional(),
+      ),
+      role: z
+        .enum([Role.CUSTOMER, Role.STORE_ADMIN, Role.SUPER_ADMIN])
+        .optional(),
+      sortBy: z.enum(USER_SORT_BY).default("createdAt"),
+      sortOrder: z.enum(USER_SORT_ORDER).default("asc"),
+    }),
+  });
+  static readonly CREATE_STORE_ADMIN = z.object({
+    body: z.object({
+      name: z.string().min(1, "Name is required"),
+      email: z.string().email("Invalid email format"),
+      password: z.string().min(6, "Password must be at least 6 characters"),
+      storeId: z.string().uuid(" Invalid input store id"),
+    }),
+  });
+  static readonly UPDATE_STORE_ADMIN = z.object({
+    params: z.object({
+      id: z.string().uuid("Invalid user id"),
+    }),
+    body: z.object({
+      name: z.string().min(1).optional(),
+      storeId: z.string().uuid().optional(),
+    }),
+  });
+  static readonly DELETE_STORE_ADMIN = z.object({
+    params: z.object({
+      id: z.string().uuid("Invalid user id"),
+    }),
+  });
+}
+export type getAllUserSchema = z.infer<typeof AdminValidation.GET_ALL_USER>;
+export type createStoreAdminSchema = z.infer<
+  typeof AdminValidation.CREATE_STORE_ADMIN
+>;
+export type updateStoreAdminSchema = z.infer<
+  typeof AdminValidation.UPDATE_STORE_ADMIN
+>;
+export type deleteStoreAdminSchema = z.infer<typeof AdminValidation.DELETE_STORE_ADMIN>
